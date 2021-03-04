b566971b94ef2544d6aa7786d3f8a4451a626746
TaiVT <>
2021-03-04 17:14:10 +0700

    Done Lecture 1 - Practice6BigSegment.java

--- Lecture1-DynamicArrayAndString/src/main/java/Practice6BigSegment.java
+++ Lecture1-DynamicArrayAndString/src/main/java/Practice6BigSegment.java
@@ -5,48 +5,24 @@ public class Practice6BigSegment {
         Scanner scanner = new Scanner(System.in);
         int nOfSegment = scanner.nextInt();
 
-        int minL = 0;
-        int maxR = 0;
-        int bigSegRow = -1;
+        int minL = Integer.MAX_VALUE;
+        int maxR = Integer.MIN_VALUE;
+        int[][] segments = new int[nOfSegment][2];
         for (int i = 0; i < nOfSegment; i++) {
             int lVal = scanner.nextInt();
             int rVal = scanner.nextInt();
+            minL = Math.min(minL, lVal);
+            maxR = Math.max(maxR, rVal);
 
-            if (bigSegRow == -1) {
-                if (i == 0) {
-                    minL = lVal;
-                    maxR = rVal;
-                }
-                if (i == nOfSegment -1) {
-                    boolean isNewBigSeg = lVal <= minL && maxR <= rVal;
-                    if (isNewBigSeg) {
-                        bigSegRow = nOfSegment - 1;
-                    } else {
-                        bigSegRow = -1;
-                    }
-                    break;
-                }
+            segments[i][0] = lVal;
+            segments[i][1] = rVal;
+        }
 
-                minL = Math.min(lVal, minL);
-                maxR = Math.max(rVal, maxR);
+        int bigSegRow = -1;
+        for (int i = 0; i < nOfSegment; i++) {
+            if (segments[i][0] == minL && segments[i][1] == maxR) {
                 bigSegRow = i + 1;
-                continue;
-            }
-
-            boolean isCurBigSegValid = minL <= lVal && rVal <= maxR;
-            if (isCurBigSegValid) {
-                continue;
-            }
-
-            boolean isNewBigSeg = lVal <= minL && maxR <= rVal;
-            if (isNewBigSeg) {
-                minL = lVal;
-                maxR = rVal;
-                bigSegRow = i + 1;
-            } else {
-                minL = Math.min(lVal, minL);
-                maxR = Math.max(rVal, maxR);
-                bigSegRow = -1;
+                break;
             }
         }
 
